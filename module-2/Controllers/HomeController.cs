e616be59e4f1abc69880fb3d98244e3a5c87e098
Misha M. <m1sha.tvink037@gmail.com>
2024-05-27 21:25:01 +0300

    Add project files.

---
+++ module-2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace module_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        public ActionResult TASK1()
+        {
+            return View();
+        }
+
+        public ActionResult TASK2()
+        {
+            return View();
+        }
+
+        public ActionResult TASK3()
+        {
+            return View();
+        }
+
+
+    }
+}
